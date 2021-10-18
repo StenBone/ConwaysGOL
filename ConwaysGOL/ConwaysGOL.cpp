01bc59f9210006d9a20b8c3dd04fd3f0288b7a01
Ben Stone <spector.8500@gmail.com>
2021-10-18 13:27:45 -0400

    Uniform initialization on locally scoped variables

--- ConwaysGOL/ConwaysGOL.cpp
+++ ConwaysGOL/ConwaysGOL.cpp
@@ -3,9 +3,9 @@
 namespace ConwaysGOL {
 
 	const std::string  APP_NAME{ "Conway's Game of Life" };
-	const sf::Vector2i APP_BOUNDS{800, 600};
+	const sf::Vector2i APP_BOUNDS{ 800, 600 };
 	const sf::Vector2i BOARD_BOUNDS{ 40, 30 };
-	const float CELL_SIZE{20.f};
+	const float CELL_SIZE{ 20.f };
 
 	enum class GAME_STATES {
 		STAGING, // Waiting for user to setup cells and press 'space' to start the game
@@ -17,8 +17,8 @@ namespace ConwaysGOL {
 	{
 		GAME_STATES game_state = GAME_STATES::STAGING;
 
-		sf::RenderWindow window(sf::VideoMode(APP_BOUNDS.x, APP_BOUNDS.y), APP_NAME);
-		sf::RectangleShape box(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+		sf::RenderWindow window{ sf::VideoMode{APP_BOUNDS.x, APP_BOUNDS.y}, APP_NAME };
+		sf::RectangleShape box{sf::Vector2f{CELL_SIZE, CELL_SIZE}};
 
 		while (window.isOpen()) {
 
@@ -30,7 +30,7 @@ namespace ConwaysGOL {
 
 			// GAME_OVER
 
-			sf::Event event;
+			sf::Event event = {};
 			while (window.pollEvent(event)) {
 				if (event.type == sf::Event::Closed) {
 					window.close();
