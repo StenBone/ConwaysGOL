b6ca3a19f1da1bdb2545236b52e268c8cf2ac261
Ben Stone <spector.8500@gmail.com>
2021-10-23 20:44:20 -0400

    Working again
    Error in the LNK2019 was that there were left over temporary build files confusing the build system.

--- ConwaysGOL/ConwaysGOL.cpp
+++ ConwaysGOL/ConwaysGOL.cpp
@@ -1,54 +1,67 @@
 #include <Vector>
 #include <SFML/Graphics.hpp>
 
-namespace ConwaysGOL {
+//namespace cgol { // Conways Game of Life
 
-	const std::string  APP_NAME{ "Conway's Game of Life" };
-	const sf::Vector2i APP_BOUNDS{ 800, 600 };
-	const sf::Vector2i BOARD_BOUNDS{ 40, 30 };
-	const float CELL_SIZE{ 20.f };
+const std::string  APP_NAME{ "Conway's Game of Life" };
+const sf::Vector2u APP_BOUNDS{ 800, 600 };
+const sf::Vector2i BOARD_BOUNDS{ 40, 30 };
+const sf::Vector2f CELL_SIZE{ 20.f, 20.f };
 
-	enum class GAME_STATES {
-		STAGING, // Waiting for user to setup cells and press 'space' to start the game
-		PLAYING, // Simulation is activiely being processed
-		GAME_OVER // Exit out of app and perform resource clean up
-	};
+enum class GAME_STATES {
+	STAGING, // Waiting for user to setup cells and press 'space' to start the game
+	PLAYING, // Simulation is activiely being processed
+	GAME_OVER // Exit out of app and perform resource clean up
+};
 
-	class Cell {};
-
-	int main()
-	{
-		GAME_STATES game_state = GAME_STATES::STAGING;
-
-		sf::RenderWindow window{ sf::VideoMode{APP_BOUNDS.x, APP_BOUNDS.y}, APP_NAME };
-		sf::RectangleShape box{sf::Vector2f{CELL_SIZE, CELL_SIZE}};
-
-		// INIT BOARD, INIT CELLS
-		std::vector<Cell> board{};
-
-		while (window.isOpen()) {
-
-			// STAGING
+struct Cell {
+	sf::RectangleShape shape{};
+};
 
 
-			// PLAYING
+//}
 
+int main()
+{
+	GAME_STATES game_state = GAME_STATES::STAGING;
 
-			// GAME_OVER
+	sf::RenderWindow window{ sf::VideoMode{APP_BOUNDS.x, APP_BOUNDS.y}, APP_NAME };
 
-			sf::Event event = {};
-			while (window.pollEvent(event)) {
-				if (event.type == sf::Event::Closed) {
-					window.close();
-				}
-			}
+	// INIT BOARD, INIT CELLS
+	std::vector<Cell> board{};
 
-			window.clear(sf::Color::Black);
-			//DRAW LINES, UNTIL IT SHOULD BE CACHED
-			//UPDATE CELLS IF IT IS TIME TO
-			window.draw(box);
-			window.display();
+	while (window.isOpen()) {
+
+		int border = 9;
+		for (int row = 0; row < 4; row++) {
+			Cell cell;
+			cell.shape.setPosition(row * CELL_SIZE.x + 9, 0);
+			cell.shape.setSize(CELL_SIZE);
+			board.push_back(cell);
 		}
-		return EXIT_SUCCESS;
-	} // BE KIND
-}
+
+
+		// STAGING
+
+
+		// PLAYING
+
+
+		// GAME_OVER
+
+		sf::Event event = {};
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		// RENDER
+		window.clear(sf::Color::Black);
+		for (Cell cell : board) {
+			window.draw(cell.shape);
+		}
+		window.display();
+	}
+	return EXIT_SUCCESS;
+} // BE KIND
