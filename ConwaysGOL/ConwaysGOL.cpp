b180199d6f76a2102541adaacadf6a963ee6dbec
Ben Stone <spector.8500@gmail.com>
2021-11-01 21:46:47 -0400

    Updated cell renderer

--- ConwaysGOL/ConwaysGOL.cpp
+++ ConwaysGOL/ConwaysGOL.cpp
@@ -3,12 +3,15 @@
 
 //namespace cgol { // Conways Game of Life
 
-const std::string  APP_NAME		{ "Conway's Game of Life" };
-const sf::Vector2u APP_BOUNDS	{ 800, 600 };
-const sf::Vector2i BOARD_BOUNDS	{ 32, 24 };
-const sf::Vector2f CELL_SIZE	{ 21.f, 21.f }; // should be a single value, always going to be square
-const float		   BORDER_WIDTH	{ 2.f };
-const int		   CELLS_HORIZONTAL = (BORDER_WIDTH + CELL_SIZE.x + BORDER_WIDTH) / APP_BOUNDS.x;
+const std::string  APP_NAME				{ "Conway's Game of Life" };
+const sf::Vector2u APP_BOUNDS			{ 800, 600 };
+const sf::Vector2i BOARD_BOUNDS			{ 32, 24 };
+const float		   CELL_SIZE			{ 21.f }; // should be a single value, always going to be square
+const float		   BORDER_SIZE			{ 2.f };
+const float		   TOTAL_BORDER_SIZE	{ 2 * BORDER_SIZE };
+const float		   BOX_SIZE				{ CELL_SIZE + TOTAL_BORDER_SIZE };
+const float		   CELLS_HORIZONTAL		{ APP_BOUNDS.x / BOX_SIZE }; // cell_index(border_left + cell_width + border_right) = cell_x
+const float		   CELLS_VERTICAL		{ APP_BOUNDS.y / BOX_SIZE }; // cell_index(border_left + cell_height + border_right) = cell_y
 
 // 800 / 25 = 32 cells @ 25 pixels across
 // 600 / 25 = 24 cells
@@ -26,16 +29,21 @@ int main()
 	// INIT BOARD, INIT CELLS
 	std::vector<Cell> board{};
 	for (int row = 0; row < CELLS_HORIZONTAL; row++) {
-		Cell cell;
-		cell.shape.setPosition(BORDER_WIDTH + row * (CELL_SIZE.x + BORDER_WIDTH), 0); // Cell position = (index(cell_size + border_width))
-		cell.shape.setSize(CELL_SIZE);
-		board.push_back(cell);
+		for (int col = 0; col < CELLS_VERTICAL; col++) {
+			Cell cell;
+			cell.shape.setPosition(
+									(row * (CELL_SIZE + TOTAL_BORDER_SIZE)) + BORDER_SIZE, 
+									(col * (CELL_SIZE + TOTAL_BORDER_SIZE)) + BORDER_SIZE
+								);
+			cell.shape.setSize(sf::Vector2f{ CELL_SIZE, CELL_SIZE });
+			board.push_back(cell);
+		}
 	}
 
 	while (window.isOpen()) {
 
 		// EVENTS
-		sf::Event event = {};
+		sf::Event event {};
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
