64c92694d73a5cb5d0c3dc58a46b39ad3b44c45e
Ben Stone <spector.8500@gmail.com>
2021-11-15 08:21:55 -0500

    Pivoting away from a 2d representation. I think I can use a vector of active cells and be careful to link cells together to their neighbors at add time, doing the processing up front.

--- ConwaysGOL/Board.h
+++ ConwaysGOL/Board.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <SFML/Graphics/Drawable.hpp>
-/* A board has a 2D matrix of generic values. Can be drawn.
+/*
+What is it? A board has a 2D matrix of generic values. Can be drawn.
+Responsibilites: Storing a vector of active cells, Drawing the background of the game board and 
+any active cells over the top.
+Inspiration: sf::Sprite
 todo templated parameters
-implement draw function*/
+implement draw function
+*/
 class Board : public Matrix2D, sf::Drawable {
 public:
 
---
+++ ConwaysGOL/Matrix2D.h
@@ -0,0 +1,8 @@
+#pragma once
+/**
+What is it? A 2D templatized container. 
+*/
+class Matrix2D
+{
+};
+
